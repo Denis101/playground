7f257d1468ac04390e48ebe19dba8200e6b3b962
Denis Craig <denis.craig@infinityworks.com>
2017-02-23 18:25:29 +0000

    Added mult-machine vagrant setup for a spark cluster. Can't quite get the slave nodes to connect to the master yet.

---
+++ spark/ansible/roles/base/files/spark-env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export SPARK_HOME=/usr/local/spark
+export JAVA_HOME=/usr
+
+export PATH=$PATH:$SPARK_HOME/bin
+
+export SPARK_WORKER_CORES=2
+export SPARK_MASTER_IP=192.168.52.201
