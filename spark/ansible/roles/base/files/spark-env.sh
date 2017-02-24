376984ab141eeefd2abf2ca14c3d179c48a553ee
Denis Craig <denis.craig@infinityworks.com>
2017-02-24 14:26:01 +0000

    Spark slaves now connecting to master correctly.

--- spark/ansible/roles/base/files/spark-env.sh
+++ spark/ansible/roles/base/files/spark-env.sh
@@ -5,5 +5,10 @@ export JAVA_HOME=/usr
 
 export PATH=$PATH:$SPARK_HOME/bin
 
+# Force IPv4
+export SPARK_DAEMON_JAVA_OPTS="-Djava.net.preferIPv4Stack=true"
+
 export SPARK_WORKER_CORES=2
-export SPARK_MASTER_IP=192.168.52.201
+export SPARK_STANDALONE_MASTER=master.spark.dev
+export SPARK_MASTER_HOST=192.168.52.201
+export SPARK_LOCAL_IP=$(ip addr show eth1 | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}')
